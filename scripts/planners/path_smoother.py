b8cdaf1d33fba5561bc1ac87746a95992de6b954
Jacob Deutsch <jacobdeutsch@utexas.edu>
2022-12-08 22:56:13 -0800

    adds HW code

--- scripts/planners/path_smoother.py
+++ scripts/planners/path_smoother.py
@@ -22,7 +22,28 @@ def compute_smoothed_traj(path, V_des, k, alpha, dt):
     Hint: Use splrep and splev from scipy.interpolate
     """
     ########## Code starts here ##########
-    raise NotImplementedError # REPLACE THIS FUNCTION WITH YOUR IMPLEMENTATION
+    dts_nom = [
+      np.linalg.norm(np.array(path[i])- path[i-1]) /V_des  for i in range(1, len(path))
+    ]
+
+    ts = np.array([0] + np.cumsum(dts_nom).tolist())
+
+   
+    t_smoothed = np.arange(0, ts[-1], dt, dtype=float)
+
+
+    tck_x = scipy.interpolate.splrep(ts, np.array(path)[:,0], w=None, k=k, s=alpha)
+    x_d = scipy.interpolate.splev(t_smoothed, tck_x, der=0)
+    xd_d = scipy.interpolate.splev(t_smoothed, tck_x, der=1)
+    xdd_d = scipy.interpolate.splev(t_smoothed, tck_x, der=2)
+
+
+    tck_y = scipy.interpolate.splrep(ts, np.array(path)[:,1], k=k, s=alpha)
+    y_d = scipy.interpolate.splev(t_smoothed, tck_y, der=0)
+    yd_d = scipy.interpolate.splev(t_smoothed, tck_y, der=1)
+    ydd_d = scipy.interpolate.splev(t_smoothed, tck_y, der=2)
+    theta_d = np.arctan2(y_d, x_d)
+    
     ########## Code ends here ##########
     traj_smoothed = np.stack([x_d, y_d, theta_d, xd_d, yd_d, xdd_d, ydd_d]).transpose()
 
