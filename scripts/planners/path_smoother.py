bc5e698bf44845d3b6c8b69e3dfc5cadb4f9e825
jacob deutsch <jacobdeutsch@utexas.edu>
2022-12-15 12:17:17 -0800

    modifies path smoother

--- scripts/planners/path_smoother.py
+++ scripts/planners/path_smoother.py
@@ -22,24 +22,27 @@ def compute_smoothed_traj(path, V_des, k, alpha, dt):
     Hint: Use splrep and splev from scipy.interpolate
     """
     ########## Code starts here ##########
-    path = np.array(path)
-    N = path.shape[0]
-    t = np.zeros(N)
-    t[1:] = np.cumsum(np.linalg.norm(path[:-1] - path[1:], axis=1) / V_des)
-    t_smoothed = np.arange(0,t[-1]+dt, dt)
-    N = t_smoothed.shape[0]
+    dts_nom = [
+      np.linalg.norm(np.array(path[i])- path[i-1]) /V_des  for i in range(1, len(path))
+    ]
 
-    tck_x = scipy.interpolate.splrep(x=t, y=path[:,0], k=k, s=alpha)
-    tck_y = scipy.interpolate.splrep(x=t, y=path[:,1], k=k, s=alpha)
-    
+    ts = np.array([0] + np.cumsum(dts_nom).tolist())
+
+   
+    t_smoothed = np.arange(0, ts[-1], dt, dtype=float)
+
+
+    tck_x = scipy.interpolate.splrep(ts, np.array(path)[:,0], w=None, k=k, s=alpha)
     x_d = scipy.interpolate.splev(t_smoothed, tck_x, der=0)
-    y_d = scipy.interpolate.splev(t_smoothed, tck_y, der=0)
     xd_d = scipy.interpolate.splev(t_smoothed, tck_x, der=1)
-    yd_d = scipy.interpolate.splev(t_smoothed, tck_y, der=1)
     xdd_d = scipy.interpolate.splev(t_smoothed, tck_x, der=2)
+
+
+    tck_y = scipy.interpolate.splrep(ts, np.array(path)[:,1], k=k, s=alpha)
+    y_d = scipy.interpolate.splev(t_smoothed, tck_y, der=0)
+    yd_d = scipy.interpolate.splev(t_smoothed, tck_y, der=1)
     ydd_d = scipy.interpolate.splev(t_smoothed, tck_y, der=2)
     theta_d = np.arctan2(yd_d, xd_d)
-    
     ########## Code ends here ##########
     traj_smoothed = np.stack([x_d, y_d, theta_d, xd_d, yd_d, xdd_d, ydd_d]).transpose()
 
